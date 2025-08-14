2f979d3df6d2def1a2638533520e5fd41429fbef
IdachristyJulious <141800940+IdachristyJulious@users.noreply.github.com>
2025-08-14 12:20:18 +0530

    Update add.c
    chaged

--- add.c
+++ add.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+int main() {
+    int num1 = 10;   // first number
+    int num2 = 20;   // second number
+    int sum = num1 + num2;
+
+    printf("First Number: %d\n", num1);
+    printf("Second Number: %d\n", num2);
+    printf("Sum = %d\n", sum);
+
+    return 0;
 }
